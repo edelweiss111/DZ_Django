ef66726664af88702fdcc712bf2f669c81caa323
Saveliy <ya.savchik2000@mail.ru>
2023-10-16 18:30:54 +0400

    web приложение

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+FILE_PATH = os.path.join('src', 'index.html')
+hostName = "localhost"
+serverPort = 8080
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class MyServer(BaseHTTPRequestHandler):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @staticmethod
+    def __get_index(filepath):
+        with open(filepath, encoding='utf-8') as f:
+            html_file = f.read()
+        return html_file
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_content = self.__get_index(FILE_PATH)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
